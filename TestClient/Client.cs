6563149c7280aabd01557eaeaebfa71b2c876c94
sweetmeatsboy <sweetmeatsboy@gmail.com>
2021-06-20 14:30:14 +0900

    no message

--- TestClient/Client.cs
+++ TestClient/Client.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using CoreNet.Jobs;
+using CoreNet.Networking;
+using CoreNet.Protocols;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,38 @@ using System.Threading.Tasks;
 
 namespace TestClient
 {
-    public class Client
+    public class Client : CoreNetwork
     {
+        public static Client Inst => new Client();
+
+        private CoreSession mSession;
+        private Dictionary<string, Worker> wDict = new Dictionary<string, Worker>();
+
+
+        public override void ReadyToStart()
+        {
+            wDict["hb"] = new Worker();
+            wDict["pkg"] = new Worker();
+        }
+
+        public override void Start()
+        {
+        }
+
+        protected override void Analizer_Ans(CoreSession _s, Packet _p)
+        {
+        }
+
+        protected override void Analizer_Noti(CoreSession _s, Packet _p)
+        {
+        }
+
+        protected override void Analizer_Req(CoreSession _s, Packet _p)
+        {
+        }
+
+        protected override void Analizer_Test(CoreSession _s, Packet _p)
+        {
+        }
     }
 }
--- TestClient/Program.cs
+++ TestClient/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestClient
@@ -10,6 +11,17 @@ namespace TestClient
     {
         static void Main(string[] args)
         {
+
+            Client.Inst.ReadyToStart();
+            Client.Inst.Start();
+
+            while (Client.Inst.isDown)
+            {
+                Thread.Sleep(1000);
+            }
+
+            Console.WriteLine("Client is Fin, press any key");
+            Console.ReadLine();
         }
     }
 }
